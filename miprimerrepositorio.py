90f1d49d7aa6e7a3530564710b4623f60e9bd7ba
solx9 <solbaezsuarez.bs@gmail.com>
2025-08-16 00:59:16 -0300

    Update miprimerrepositorio.py

--- miprimerrepositorio.py
+++ miprimerrepositorio.py
@@ -3,9 +3,16 @@ num2 = float(input("Ingrese el segundo número: "))
 
 suma = num1 + num2
 resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2 if num2 != 0 else "Error: División por cero"
 
 print(f"Suma: {suma}")
 print(f"Resta: {resta}")
+<<<<<<< HEAD
 
 #calculadora solo suma y resta
 #que
+=======
+print(f"Multiplicación: {multiplicacion}")
+print(f"División: {division}")
+>>>>>>> parent of 5ac6588 (eliminación de div y mult)
